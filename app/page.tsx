bee59fc3a111cda77eb2139d5455dafe200185df
Tom <tbvanreijn@gmail.com>
2024-05-20 18:39:20 +0200

    hero

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Link from "next/link";
 import { redirect } from "next/navigation";
-import Keen from "@/components/Keen";
+import Image from "next/image";
+import hero2 from "/public/hero2.png";
+import FAQ from "@/components/Faq";
 import { Button } from "@/components/ui/button";
 import ExplainerSection from "@/components/ExplainerSection";
 import PricingSection from "@/components/PricingSection";
-import Image from "next/image";
-import FAQ from "@/components/Faq";
 
 export const dynamic = "force-dynamic";
 
@@ -23,42 +23,36 @@ export default async function Index() {
   }
 
   return (
-    <div className="flex max-w-6xl mx-auto flex-col items-center justify-center  min-h-screen">
-      <main className="flex flex-1 w-full flex-col items-center justify-center text-center  sm:mt-20  background-gradient">
-        <h1 className="mx-auto max-w-4xl font-display text-4xl font-semibold tracking-normal text-black sm:text-6xl">
-          Professionele Portetfoto's{" "}
-          <span className="relative whitespace-nowrap">
-            <span className="relative"> met behulp van AI✨</span>
-          </span>{" "}
-        </h1>
-        <h2 className="mx-auto mt-4 max-w-xl text-lg sm:text-gray-800  text-gray-800 leading-7">
-          Vergeet studio's en fotosessies! Genereer professionele Portetfoto's
-          in HD studio kwaliteit makkelijk vanaf thuis of je werk.
-        </h2>
-        <Link
-          className="bg-sky-600 rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-blue-500 transition"
-          href="/dream">
-          Begin fotosessie{" "}
-        </Link>
-        <div className="flex justify-between items-center w-full flex-col sm:mt-10 ">
-          <div className="flex flex-col space-y-10 mt-4 ">
-            <div className="flex sm:space-x-8 sm:flex-row flex-col"></div>
-          </div>{" "}
+    <div className="flex flex-col px-6 items-center ">
+      <div className="flex flex-col lg:flex-row items-center  max-w-6xl w-full">
+        <div className="flex flex-col space-y-4 lg:w-1/2 w-full">
+          <h1 className="text-6xl font-bold">
+            Zakelijke Portetfoto's met behulp van AI✨
+          </h1>
+          <p className="text-gray-600 text-lg">
+            Vergeet studio's en fotosessies! Genereer professionele Portetfoto's
+            in HD studio kwaliteit makkelijk vanaf thuis of je werk.
+          </p>
+          <div className="flex flex-col space-y-2">
+            <Link href="/login">
+              <Button className="border rounded-full  h-12 px-6 m-2 text-lg text-white bg-sky-600 hover:bg-sky-700 w-full lg:w-1/2">
+                Begin AI fotosessie{" "}
+              </Button>
+            </Link>
+          </div>
+        </div>
+        <div className="lg:w-1/2  sm:pb8 pb-16 w-full ">
           <Image
-            alt="Original photo of a room with roomGPT.io"
             src="/hero.png"
-            className="pt-0 "
-            width={900}
-            height={200}
-            priority
+            width={600}
+            height={1000}
+            alt="Picture of the author"
           />
-        </div>{" "}
-      </main>
-      <div className="mt-20 https://www.tailwindcss-animated.com/configurator.html?animation=bounce&count=once">
-        {" "}
-        <ExplainerSection />
+        </div>
       </div>
-      <PricingSection /> <FAQ />
+      <ExplainerSection />
+      <PricingSection />
+      <FAQ />
     </div>
   );
 }
--- components/ExplainerSection.tsx
+++ components/ExplainerSection.tsx
@@ -1,26 +1,29 @@
-import blur from "/public/blur.png";
-import example from "/public/example.png";
-import result from "/public/result.png";
+import voorbeeld from "/public/voorbeeld.png";
+import wazig from "/public/wazig.png";
+import resultaat from "/public/resultaat.png";
 
 export default function ExplainerSection() {
   return (
-    <div className="w-full max-w-6xl mt-16 p-8 bg-gray-100 rounded-lg space-y-8">
-      <h2 className="text-3xl font-semibold text-center mb-8">zo werkt het</h2>
+    <div
+      id="Zo werkt het"
+      className="w-full max-w-6xl mt-16  px-6 p-8 bg-gray-100 rounded-lg space-y-8">
+      <h2 className="text-3xl  font-bold text-center mb-8">Zo werkt het</h2>
 
       {/* Step 1: Upload your images */}
       <div className="space-y-4">
         <div className="flex items-center justify-center space-x-4">
-          <div className="text-3xl font-bold text-blue-600 bg-white border-2 border-blue-600 rounded-full w-10 h-10 flex items-center justify-center">
+          <div className="text-3xl font-bold text-sky-600 bg-white border-2 border-sky-600 rounded-full w-10 h-10 flex items-center justify-center">
             1
           </div>
-          <h3 className="text-2xl font-semibold">Upload een paar selfies</h3>
+          <h3 className="text-2xl font-semibold">Upload je selfies</h3>
         </div>
-        <p className="text-sm text-gray-600 text-center">
-          Upload 4-10 high-quality selfies: naar de camera kijkend, 1 persoon,
-          geen zonnebrillen of hoeden.
+        <p className="text-l text-gray-600 text-center">
+          kijk recht in de camera, zorg dat je gezicht in het midden van de foto
+          is <br />
+          vermijd hoeden en zonnebrillen.
         </p>
         <img
-          src={example.src}
+          src={voorbeeld.src}
           alt="AI Headshot example"
           className="rounded-lg object-cover w-full md:w-3/4 lg:w-1/2 mx-auto"
         />
@@ -29,19 +32,17 @@ export default function ExplainerSection() {
       {/* Step 2: Train your model */}
       <div className="space-y-4">
         <div className="flex items-center justify-center space-x-4">
-          <div className="text-3xl font-bold text-blue-600 bg-white border-2 border-blue-600 rounded-full w-10 h-10 flex items-center justify-center">
+          <div className="text-3xl font-bold text-sky-600 bg-white border-2 border-sky-600 rounded-full w-10 h-10 flex items-center justify-center">
             2
           </div>
-          <h3 className="text-2xl font-semibold">
-            Onze AI gets gaat aan het werk
-          </h3>
+          <h3 className="text-2xl font-semibold">Onze AI gaat aan het werk</h3>
         </div>
-        <p className="text-sm text-gray-600 text-center">
-          De AI is ongeveer ~20 minuten bezig. Je krijgt een email wanneer je
-          foto's klaar zijn!
+        <p className="text-l text-gray-600 text-center">
+          De AI is 15 minuten bezig wanneer het afgerond is ontvang je een
+          mailtje{" "}
         </p>
         <img
-          src={blur.src}
+          src={wazig.src}
           alt="AI Headshot blur"
           className="rounded-lg object-cover w-full md:w-3/4 lg:w-1/2 mx-auto"
         />
@@ -50,16 +51,19 @@ export default function ExplainerSection() {
       {/* Step 3: Generate images */}
       <div className="space-y-4">
         <div className="flex items-center justify-center space-x-4">
-          <div className="text-3xl font-bold text-blue-600 bg-white border-2 border-blue-600 rounded-full w-10 h-10 flex items-center justify-center">
+          <div className="text-3xl font-bold text-sky-600 bg-white border-2 border-sky-600 rounded-full w-10 h-10 flex items-center justify-center">
             3
           </div>
-          <h3 className="text-2xl font-semibold">Get amazing headshots</h3>
+          <h3 className="text-2xl font-semibold">
+            Selecteer je portretfoto's{" "}
+          </h3>
         </div>
-        <p className="text-sm text-gray-600 text-center">
-          Once your model is trained, we'll give you amazing headshots!
+        <p className="text-l text-gray-600 text-center">
+          Je ontvangt verschillende achtergronden, poses en stijlen <br /> om de
+          perfecte AI-portretfoto te kiezen.
         </p>
         <img
-          src={result.src}
+          src={resultaat.src}
           alt="AI Headshot result"
           className="rounded-lg object-cover w-full md:w-3/4 lg:w-1/2 mx-auto"
         />
