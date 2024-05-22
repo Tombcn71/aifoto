afcb805eb7a8275e142dbbf372bece74ec9fd9dd
Tom <tbvanreijn@gmail.com>
2024-05-22 14:43:31 +0200

    stripe table

--- components/ExplainerSection.tsx
+++ components/ExplainerSection.tsx
@@ -7,7 +7,8 @@ export default function ExplainerSection() {
     <div
       id="Zo werkt het"
       className="w-full max-w-6xl mt-16  px-6 p-8 bg-gray-100 rounded-lg space-y-8">
-      {/* Step 1: Upload your images */}
+      {/* Step 1: Upload your images */}{" "}
+      <h2 className="text-3xl  font-bold text-center mb-8">Hoe werkt het?</h2>
       <div className="space-y-4">
         <div className="flex items-center justify-center space-x-4">
           <div className="text-3xl font-bold text-sky-600 bg-white border-2 border-sky-600 rounded-full w-10 h-10 flex items-center justify-center">
@@ -16,9 +17,14 @@ export default function ExplainerSection() {
           <h3 className="text-2xl font-semibold">Upload 4-10 selfies</h3>
         </div>
         <p className="text-l text-gray-600 text-center">
-          kijk recht in de camera, zorg dat je gezicht in het midden van de foto
+          Kijk recht in de camera, zorg dat je gezicht in het midden van de foto
           is <br />
-          vermijd hoeden en zonnebrillen.
+          vermijd hoeden en zonnebrillen. Zorg voor gevarieerde foto's met
+          verschillende
+          <br />
+          achtergronden en situaties, bv vakantiefoto's en foto's van jouw in
+          <br />
+          het park, verjaardagen etc.
         </p>
         <img
           src={voorbeeld.src}
@@ -26,7 +32,6 @@ export default function ExplainerSection() {
           className="rounded-lg object-cover w-full md:w-3/4 lg:w-1/2 mx-auto"
         />
       </div>
-
       {/* Step 2: Train your model */}
       <div className="space-y-4">
         <div className="flex items-center justify-center space-x-4">
@@ -45,7 +50,6 @@ export default function ExplainerSection() {
           className="rounded-lg object-cover w-full md:w-3/4 lg:w-1/2 mx-auto"
         />
       </div>
-
       {/* Step 3: Generate images */}
       <div className="space-y-4">
         <div className="flex items-center justify-center space-x-4">
--- components/stripe/StripeTable.tsx
+++ components/stripe/StripeTable.tsx
@@ -1,27 +1,31 @@
-'use client'
-import { User } from '@supabase/supabase-js';
-import React, { useEffect } from 'react';
+"use client";
+import { User } from "@supabase/supabase-js";
+import React, { useEffect } from "react";
 
-interface StripePricingTableProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
-  'pricing-table-id': string;
-  'publishable-key': string;
+interface StripePricingTableProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLElement>,
+    HTMLElement
+  > {
+  "pricing-table-id": string;
+  "publishable-key": string;
 }
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      'stripe-pricing-table': StripePricingTableProps;
+      "stripe-pricing-table": StripePricingTableProps;
     }
   }
 }
 
 type Props = {
   user: User;
-}
+};
 
 const StripePricingTable = ({ user }: Props) => {
   useEffect(() => {
-    const script = document.createElement('script');
+    const script = document.createElement("script");
     script.src = "https://js.stripe.com/v3/pricing-table.js";
     script.async = true;
 
@@ -29,20 +33,18 @@ const StripePricingTable = ({ user }: Props) => {
 
     return () => {
       document.body.removeChild(script);
-    }
+    };
   }, []);
 
   return (
-    <div className='flex flex-1 flex-col w-full'>
+    <div className="flex flex-1 flex-col w-full">
       <stripe-pricing-table
-          pricing-table-id="prctbl_1P0TL0C3ic5Sd20TGpWOU2Fi"
-          publishable-key="pk_live_51P0SikC3ic5Sd20T9QRaRKIkqy8l951LDgeOxcP24ZRXHnQzjnOFM7tfhsYdWksn1wNBdejJzvaxXGq0yRAxm14A00Py0XreGk"
-          client-reference-id={user.id}
-          customer-email={user.email}
-      >
-      </stripe-pricing-table>
+        pricing-table-id="prctbl_1P9ilUFHQxgFOGeCtNu1RbnG"
+        publishable-key="pk_live_51O5QeBFHQxgFOGeCAf2jRzTU8BdbX8Ec3AwOZMeJFW89IlrzHuks9iFlvITh3ZArfZnofo8kxf0tBOU9kI02aGrV00cvwBdBYb"
+        client-reference-id={user.id}
+        customer-email={user.email}></stripe-pricing-table>
     </div>
   );
-}
+};
 
 export default StripePricingTable;
