81cf2cbf212e6620e9bab54370b6bade81f2c48d
Tom <tbvanreijn@gmail.com>
2024-06-08 21:16:49 +0200

    text

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Link from "next/link";
 import { redirect } from "next/navigation";
-import Image from "next/image";
-import hero9 from "/public/hero9.png";
-import FAQ from "@/components/Faq";
+import Faq from "@/components/Faq";
+import hero2 from "/public/hero2.png";
 import { Button } from "@/components/ui/button";
 import ExplainerSection from "@/components/ExplainerSection";
 import PricingSection from "@/components/PricingSection";
+
 export const dynamic = "force-dynamic";
 
 export default async function Index() {
@@ -22,36 +22,37 @@ export default async function Index() {
   }
 
   return (
-    <div className="flex max-w-6xl mx-auto flex-col items-center justify-center pt-12 min-h-screen">
-      <main className="flex flex-1 w-full flex-col items-center justify-center text-center pb-20 px-4 sm:mt20  ">
-        <h1 className="mx-auto max-w-4xl font-display text-4xl sm:text-6xl text-slate-800 font-extrabold tracking-normal ">
-          Professionele Portetfoto's maken met AI✨
-        </h1>
-        <h2 className="mx-auto mt-6 mb-6 max-w-xl text-lg sm:text-gray-800  text-gray-800 leading-7">
-          Vergeet studios en fotosessies! Genereer professionele Portetfoto's in
-          HD studio kwaliteit makkelijk vanaf thuis of je werk.
-        </h2>
-        <Link href="/login">
-          {" "}
-          <Button className="border rounded-full h-14 px-10 m-2 text-xl text-white hover:bg-sky-600 bg-sky-600">
-            Begin jouw AI fotosessie
-          </Button>
-        </Link>
-
-        <div className="flex justify-between items-center pt-20 w-full flex-col sm:mt-10 mt-6">
-          <div></div>
-          <ExplainerSection />
-
-          <div className="flex flex-col space-y-10 mt-4 mb-16">
-            <div className="flex sm:space-x-8 sm:flex-row flex-col"></div>
+    <div className="flex flex-col items-center pt-16">
+      <div className="flex flex-col lg:flex-row items-center gap-8 p-8 max-w-6xl w-full">
+        <div className="flex flex-col space-y-4 lg:w-1/2 w-full">
+          <h1 className="text-5xl font-bold ">
+            Zakelijke Portetfoto's <br />
+            met behulp van AI✨
+          </h1>
+          <p className="text-gray-600 text-xl">
+            Vergeet dure studio's en ongemakkelijke fotosessies! <br />
+            Genereer professionele zakelijke Portetfoto's in HD studio kwaliteit
+            die veel goedkoper zijn en geen tijd en reizen in beslag nemen,
+            makkelijk vanaf thuis of je werk.
+          </p>
+          <div className="flex flex-col space-y-2">
+            <Link href="/login">
+              <Button className="border rounded-full  h-12 px-6 m-2 text-lg text-white bg-sky-600 hover:bg-sky-700 w-full lg:w-1/2">
+                Begin A.I. fotosessie{" "}
+              </Button>
+            </Link>
           </div>
         </div>
-      </main>
-      <FAQ />
-      <div>
-        {" "}
-        <PricingSection />
+        <div className="lg:w-1/2 w-full mt-8 lg:mt-0">
+          <img
+            src={hero2.src}
+            alt="AI Headshot Illustration"
+            className="rounded-lg object-cover w-full h-full"
+          />
+        </div>
       </div>
+      <ExplainerSection />
+      <PricingSection /> <Faq />
     </div>
   );
 }
